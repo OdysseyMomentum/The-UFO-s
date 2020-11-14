07e05da0a30133fb3b36ba7971ad4c5c86977361
Lana <svetlana.konstantinovna@gmail.com>
2020-11-14 21:59:49 +0100

    revert price calc

--- utils/uniswap_functions.py
+++ utils/uniswap_functions.py
@@ -42,7 +42,8 @@ def bump_price(target_price_cents):
 	# we need to buy KWH
 	if target_price > 1.02*current_price:
 		logging.info("Current price of KWH too low")
-		amount_to_sell_eth = eth_bal*(target_price - current_price)/(current_price + target_price)
+		percentage = 0.001
+		amount_to_sell_eth = eth_bal*percentage
 		tx = uniswap_wrapper.make_trade(eth, kwh, int(amount_to_sell_eth)).hex()
 		logging.info(tx)
 		uniswap_wrapper.w3.eth.waitForTransactionReceipt(tx)
@@ -56,7 +57,8 @@ def bump_price(target_price_cents):
 	# we need to buy ETH
 	elif target_price < 0.98*current_price:
 		logging.info("Current price of KWH too high")
-		amount_to_sell_kwh = kwh_bal*(current_price - target_price)/(current_price + target_price)
+		percentage = 0.001
+		amount_to_sell_kwh = kwh_bal*percentage
 		tx = uniswap_wrapper.make_trade(kwh, eth, int(amount_to_sell_kwh)).hex()
 		logging.info(tx)
 		uniswap_wrapper.w3.eth.waitForTransactionReceipt(tx)
